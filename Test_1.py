080572eff84bcc07fcf60e0589c98f0f978b3267
yueyuelaojun680319 <usicdiet@126.com>
2022-05-07 10:57:55 +0800

    Add products.py

---
+++ Test_1.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+    name = input('请输入商品名称：')
+    if name == 'q':
+        break
+    price = input('请输入价格：')
+    # p = []
+    # p.append(name)
+    # p.append(price)
+    #p = [name, price]#简洁写法
+    #products.append(p)
+    products.append([name,price])#最最简洁的写法！
+print(products)
