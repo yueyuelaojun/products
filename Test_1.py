d3dc98266ebdf8a54c517ab5eb0dd0a70cec675d
yueyuelaojun680319 <usicdiet@126.com>
2022-05-07 23:54:27 +0800

    Add write file prodoctus.csv

--- Test_1.py
+++ Test_1.py
@@ -4,6 +4,7 @@ while True:
     if name == 'q':
         break
     price = input('请输入价格：')
+    price = int(price)
     # p = []
     # p.append(name)
     # p.append(price)
@@ -14,3 +15,8 @@ print(products)
 
 for p in products:
     print(p[0], '的价格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,价格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
