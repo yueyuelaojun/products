6a6baf5dd807edb1517b87bda91c9015644ff4dd
yueyuelaojun680319 <usicdiet@126.com>
2022-05-08 11:15:21 +0800

    Add Read File

--- Test_1.py
+++ Test_1.py
@@ -1,4 +1,14 @@
+# 读取档案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,价格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
+# 让使用者输入数据
+# products = []
 while True:
     name = input('请输入商品名称：')
     if name == 'q':
@@ -12,10 +22,10 @@ while True:
     # products.append(p)
     products.append([name, price])  # 最最简洁的写法！
 print(products)
-
+# 印出商品名称&价格列表
 for p in products:
     print(p[0], '的价格是', p[1])
-
+# 写入档案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,价格\n')
     for p in products:
