dbe5344fe01366aa7607bd120626f06225d911b4
yueyuelaojun680319 <usicdiet@126.com>
2022-05-08 12:03:12 +0800

    Add check file commit

--- Test_1.py
+++ Test_1.py
@@ -1,12 +1,19 @@
+import os #operating system
+
 # 读取档案
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,价格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):#检查档案是否存在
+    print('yeah!找到档案了！')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,价格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到档案......')
+
 # 让使用者输入数据
 # products = []
 while True:
